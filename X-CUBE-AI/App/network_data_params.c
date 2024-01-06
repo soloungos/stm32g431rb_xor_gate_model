235b8e019ea64644a02a4f6039df2be5dfd04c9b
kangc <kangc@DESKTOP-P3Q81SU>
2024-01-06 20:55:59 +0900

    init

---
+++ X-CUBE-AI/App/network_data_params.c
@@ -0,0 +1,43 @@
+/**
+  ******************************************************************************
+  * @file    network_data_params.c
+  * @author  AST Embedded Analytics Research Platform
+  * @date    Sat Jan  6 13:57:57 2024
+  * @brief   AI Tool Automatic Code Generator for Embedded NN computing
+  ******************************************************************************
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  ******************************************************************************
+  */
+
+#include "network_data_params.h"
+
+
+/**  Activations Section  ****************************************************/
+ai_handle g_network_activations_table[1 + 2] = {
+  AI_HANDLE_PTR(AI_MAGIC_MARKER),
+  AI_HANDLE_PTR(NULL),
+  AI_HANDLE_PTR(AI_MAGIC_MARKER),
+};
+
+
+
+
+/**  Weights Section  ********************************************************/
+AI_ALIGNED(32)
+const ai_u64 s_network_weights_array_u64[5] = {
+  0xc06fcb80c06f746dU, 0xc0b4a6c8c0b3a52dU, 0x4004348640afcaf6U, 0xc0f3573d40e6fe62U,
+  0xc050cbdaU,
+};
+
+
+ai_handle g_network_weights_table[1 + 2] = {
+  AI_HANDLE_PTR(AI_MAGIC_MARKER),
+  AI_HANDLE_PTR(s_network_weights_array_u64),
+  AI_HANDLE_PTR(AI_MAGIC_MARKER),
+};
+
